21f33d1ca1013a22468ef6f75fdde1ed45e70d7c
sumanta <sumanta111195@gmail.com>
2020-05-13 20:00:46 +0530

    newsMVVM

--- app/src/main/java/com/sumanta/newsmvvm/repository/NewsRepository.kt
+++ app/src/main/java/com/sumanta/newsmvvm/repository/NewsRepository.kt
@@ -1,8 +1,12 @@
 package com.sumanta.newsmvvm.repository
 
 import com.sumanta.newsmvvm.db.ArticleDatabase
+import com.sumanta.newsmvvm.network.api.RetrofitInstance
 
 class NewsRepository(
     val db: ArticleDatabase
 ) {
+
+    suspend fun getBreakingNews(countryCode: String, PageNumber: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode, PageNumber)
 }
--- app/src/main/java/com/sumanta/newsmvvm/ui/NewsViewModel.kt
+++ app/src/main/java/com/sumanta/newsmvvm/ui/NewsViewModel.kt
@@ -1,9 +1,38 @@
 package com.sumanta.newsmvvm.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sumanta.newsmvvm.network.response.NewsResponse
 import com.sumanta.newsmvvm.repository.NewsRepository
+import com.sumanta.newsmvvm.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class NewsViewModel(
     val newsRepository: NewsRepository
 ): ViewModel() {
+
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var breakingNewsPage = 1
+
+    init {
+        getBreakingNews("in")
+    }
+
+    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val resource = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
+        breakingNews.postValue(handleBreakNewsResponse(resource))
+    }
+
+    private fun handleBreakNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse>{
+        if (response.isSuccessful){
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
 }
