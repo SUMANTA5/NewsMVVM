a1a352f1c41f4601ebc1173d1e9099687cdc311c
sumanta <sumanta111195@gmail.com>
2020-05-14 19:03:44 +0530

    newsMVVM

--- app/src/main/java/com/sumanta/newsmvvm/ui/fragment/ArticleFragment.kt
+++ app/src/main/java/com/sumanta/newsmvvm/ui/fragment/ArticleFragment.kt
@@ -1,7 +1,32 @@
 package com.sumanta.newsmvvm.ui.fragment
 
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.navigation.fragment.navArgs
 import com.sumanta.newsmvvm.R
+import com.sumanta.newsmvvm.ui.NewsActivity
+import kotlinx.android.synthetic.main.fragment_article.*
+import kotlinx.android.synthetic.main.item_article_preview.*
 
 class ArticleFragment: Fragment(R.layout.fragment_article) {
+
+    lateinit var viewModel: ViewModel
+    val args: ArticleFragmentArgs by navArgs()
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel  = (activity as NewsActivity).viewModel
+
+        val article = args.article
+        webView.apply {
+            webViewClient = WebViewClient()
+            loadUrl(article.url)
+        }
+
+    }
+
 }
